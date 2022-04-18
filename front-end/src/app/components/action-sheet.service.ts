93734a05342412360edcdc9c97c63c70f81f2ecc
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2022-04-18 11:53:17 -0300

    Criação da componente action sheet

---
+++ front-end/src/app/components/action-sheet.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActionSheetService {
+
+  constructor(public actionSheetController: ActionSheetController) { }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        data: 'Data value',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
+}
